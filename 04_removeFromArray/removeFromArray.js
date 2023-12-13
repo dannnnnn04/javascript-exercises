7027a7a17ab0d4749cd7981b0f66ecc8f4eb90a9
Daniel_J Carig <24857822+dannnnnn04@users.noreply.github.com>
2023-12-13 14:28:44 -0800

    Add solution to 04_removeFromArray/ exercise
    removeFromArray(): a function that takes an array and some other arguments then removes the other arguments from that array
    -used rest parameter syntax to allow the function to accept an indefinite number of arguments as array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const newArray = [];
+    array.forEach(myFunction);
+
+    function myFunction(item) {
+        if(!args.includes(item)){ //if the current item in "array" doesn't match any of of the element in "args"
+            newArray.push(item);  //... include it in our newArray
+        }
+    }
+
+    return newArray;
 
 };
 
